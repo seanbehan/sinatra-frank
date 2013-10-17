5cea9054187fc8319302924502ee96b196f0f5b4
Sean Behan <inbox@seanbehan.com>
2013-10-17 19:47:09 -0400

    honeypot

--- lib/sinatra/frank.rb
+++ lib/sinatra/frank.rb
@@ -3,48 +3,59 @@ require "sinatra/base"
 
 module Sinatra
   module Frank
-    def render_with(template, opts={})
-      erb(:"#{template}", { layout: :layout }.merge(opts))
-    end
-
-    def partial(template, opts={})
-      parts = template.split('/')
-      last  = "_#{parts.pop}"
-      erb([parts, last].flatten.join('/').to_sym, {layout: false}.merge(opts))
-    end
-
-    def url_for(*models)
-      '/' + models.map do |m|
-        [m.class.name.pluralize.downcase, m.to_param]
-      end.join('/')
-    end
-
-    def time_in_words(time)
-      s = Time.now.to_i - time.to_i
-
-      x = if s > 29030400
-        [(s/29030400), 'years']
-      elsif s > 2419200
-        [(s/2419200), 'months']
-      elsif s > 604800
-        [(s/604800), 'weeks']
-      elsif s > 86400
-        [(s/86400), 'days']
-      elsif s > 3600
-        [(s/3600), 'hours']
-      elsif s > 60
-        [(s/60), 'minutes']
-      else
-        [s, 'seconds']
+    module Helpers
+      def render_with(template, opts={})
+        erb(:"#{template}", { layout: :layout }.merge(opts))
       end
 
-      if x[0] == 1
-        x.join(' ')[0...-1]
-      else
-        x.join(' ')
+      def partial(template, opts={})
+        parts = template.split('/')
+        last  = "_#{parts.pop}"
+        erb([parts, last].flatten.join('/').to_sym, {layout: false}.merge(opts))
+      end
+
+      def url_for(*models)
+        '/' + models.map do |m|
+          [m.class.name.pluralize.downcase, m.to_param]
+        end.join('/')
+      end
+
+      def time_in_words(time)
+        s = Time.now.to_i - time.to_i
+
+        x = if s > 29030400
+          [(s/29030400), 'years']
+        elsif s > 2419200
+          [(s/2419200), 'months']
+        elsif s > 604800
+          [(s/604800), 'weeks']
+        elsif s > 86400
+          [(s/86400), 'days']
+        elsif s > 3600
+          [(s/3600), 'hours']
+        elsif s > 60
+          [(s/60), 'minutes']
+        else
+          [s, 'seconds']
+        end
+
+        if x[0] == 1
+          x.join(' ')[0...-1]
+        else
+          x.join(' ')
+        end
       end
     end
   end
 
+  module HoneyPot
+    def honey_pot(parameter)
+      before {
+        halt 403, "So sorry but something went wrong." unless request.params[parameter].empty?
+      }
+    end
+  end
+
   helpers Frank
+  register HoneyPot
 end
